3bb5f5f097cef4a5bb4c86de59b97ffa93a57a50
Supun Hettigoda <hettigoda.supun@gmail.com>
2023-03-29 01:47:51 +1100

    [EX-1]: added new taskRepository method loadByTitle().

--- src/pages/api/dao/taskRepository.test.ts
+++ src/pages/api/dao/taskRepository.test.ts
@@ -1,10 +1,68 @@
-import { loadAll as loadAllTasks } from './taskRepository'
+import { loadAll as loadAllTasks, loadByTitle } from './taskRepository'
 
+// this test suits depends on the init data.
 describe('Task repository tests', () => {
-    // this test depends on the init data.
-    it('Load all tasks', async () => {
+
+    it('load all tasks', async () => {
         const tasks = await loadAllTasks();
         expect(tasks.totalCount).toBe(9);
-        expect(tasks.tasks.length).toBe(9)
-    })
-})
+        expect(tasks.tasks.length).toBe(9);
+    });
+
+    describe('Load by title tests', () => {
+        it('when empty match to all', async () => {
+            const tasks = await loadByTitle("");
+            expect(tasks.totalCount).toBe(9);
+            expect(tasks.tasks.length).toBe(9);
+        });
+
+        it('when match at start', async () => {
+            const tasks = await loadByTitle("Deploy");
+            expect(tasks.totalCount).toBe(2);
+            expect(tasks.tasks.length).toBe(2);
+            expect(tasks.tasks[0].title).toMatch(/deploy/i);
+        });
+
+        it('when match at middle', async () => {
+            const tasks = await loadByTitle("unit");
+            expect(tasks.totalCount).toBe(1);
+            expect(tasks.tasks.length).toBe(1);
+        });
+
+        it('when match at end', async () => {
+            const tasks = await loadByTitle("testing");
+            expect(tasks.totalCount).toBe(2);
+            expect(tasks.tasks.length).toBe(2);
+        });
+
+        it('when match anywhere', async () => {
+            const tasks = await loadByTitle("test");
+            expect(tasks.totalCount).toBe(3);
+            expect(tasks.tasks.length).toBe(3);
+        });
+
+        it('when no match', async () => {
+            const tasks = await loadByTitle("something not exist");
+            expect(tasks.totalCount).toBe(0);
+            expect(tasks.tasks.length).toBe(0);
+        });
+
+        it('when white sapce', async () => {
+            const tasks = await loadByTitle("Deploy to");
+            expect(tasks.totalCount).toBe(2);
+            expect(tasks.tasks.length).toBe(2);
+        });
+
+        it('verify case insensitive match', async () => {
+            const tasks = await loadByTitle("dePloy to");
+            expect(tasks.totalCount).toBe(2);
+            expect(tasks.tasks.length).toBe(2);
+        });
+
+        it('full match', async () => {
+            const tasks = await loadByTitle("Review copy");
+            expect(tasks.totalCount).toBe(1);
+            expect(tasks.tasks.length).toBe(1);
+        });
+    });
+});
--- src/pages/api/dao/taskRepository.ts
+++ src/pages/api/dao/taskRepository.ts
@@ -1,4 +1,5 @@
 import { withDatabaseConnection } from "../../../database/database";
+import { DataTypeOIDs, QueryResult } from 'postgresql-client';
 
 export const loadAll = async () => {
     return withDatabaseConnection(async (connection) => {
@@ -17,3 +18,32 @@ export const loadAll = async () => {
         };
     });
 };
+
+export const loadByTitle = async (title: String) => {
+    return withDatabaseConnection(async (connection) => {
+        const statement = await connection.prepare(
+            "SELECT id, title, done " +
+            "FROM tasks " +
+            "WHERE title ilike $1 " +
+            "ORDER BY id ASC",
+            { paramTypes: [DataTypeOIDs.varchar] });
+
+        const tasksQueryResult = await statement.execute({ params: ["%" + title + "%"] });
+        await statement.close();
+        return mapTaskProperties(tasksQueryResult);
+    });
+};
+
+function mapTaskProperties(tasksQueryResult: QueryResult) {
+    const tasks = tasksQueryResult.rows?.map(row => ({
+        id: row[0],
+        title: row[1],
+        done: row[2],
+    })) || [];
+
+    return {
+        tasks,
+        totalCount: tasks?.length || 0
+    };
+}
+
