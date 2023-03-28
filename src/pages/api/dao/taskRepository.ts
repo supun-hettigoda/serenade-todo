28ba880ad3c2a04843003a7d2c7262eb56e49718
Supun Hettigoda <hettigoda.supun@gmail.com>
2023-03-29 01:52:49 +1100

    [EX-1]: refactored the existing loadAll() method.

--- src/pages/api/dao/taskRepository.ts
+++ src/pages/api/dao/taskRepository.ts
@@ -3,19 +3,11 @@ import { DataTypeOIDs, QueryResult } from 'postgresql-client';
 
 export const loadAll = async () => {
     return withDatabaseConnection(async (connection) => {
-        const tasksQueryResult = await connection.query('select id, title, done from tasks order by id asc');
-        const countResult = await connection.query('select count(*) from tasks');
-
-        const tasks = tasksQueryResult.rows?.map(row => ({
-            id: row[0],
-            title: row[1],
-            done: row[2],
-        })) || [];
-
-        return {
-            tasks,
-            totalCount: countResult.rows![0][0]
-        };
+        const tasksQueryResult = await connection.query(
+            'SELECT id, title, done ' +
+            'FROM tasks ' +
+            'ORDER BY id ASC');
+        return mapTaskProperties(tasksQueryResult);
     });
 };
 
